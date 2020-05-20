7a7f94659d47ef776ff378dc93da29f720b75d86
nalansitan <nalansitan@gmail.com>
2020-05-20 22:16:33 +0800

    extract backstage_pass sell_in < 0

--- src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/GildedRose.java
@@ -6,6 +6,7 @@ class GildedRose {
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final int MIN_QUALITY = 0;
     public static final int MAX_QUALITY = 50;
+    public static final int MIN_SELLIN = 0;
     Goods[] goods;
 
     public GildedRose(Goods[] goods) {
@@ -33,7 +34,7 @@ class GildedRose {
                 goods[i].sell_in = goods[i].sell_in - 1;
             }
 
-            if (goods[i].sell_in < MIN_QUALITY) {
+            if (goods[i].sell_in < MIN_SELLIN) {
                 if (!goods[i].name.equals(AGED_BRIE)) {
                     if (!goods[i].name.equals(BACKSTAGE_PASSES)) {
                         if (goods[i].quality > MIN_QUALITY) {
@@ -41,8 +42,6 @@ class GildedRose {
                                 goods[i].quality = goods[i].quality - 1;
                             }
                         }
-                    } else {
-                        goods[i].quality = MIN_QUALITY;
                     }
                 } else {
                     if (goods[i].quality < MAX_QUALITY) {
@@ -65,6 +64,9 @@ class GildedRose {
             good.quality = MAX_QUALITY;
         }
         good.sell_in -=1;
+        if (good.sell_in < MIN_SELLIN){
+            good.quality = MIN_QUALITY;
+        }
     }
 
     private void calculate_aged_brie(Goods good){
